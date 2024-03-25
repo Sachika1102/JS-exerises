6154519f3f74097cf4baf1cf4b3a82889c081d55
sachika <s.tsukeyanagi.sys23@morijyobi.ac.jp>
2024-03-25 22:24:59 +0900

    課題

---
+++ practice_6/practice_6.js
@@ -0,0 +1,28 @@
+"use strict";
+function btnClick(){
+    let wordValue = document.getElementById("wordInput").value;
+    let result = document.getElementById("result");
+    result.textContent = wordValue + "";
+}
+
+let changeText = document.getElementById("changeText");
+
+function btnClick2(){
+    clear.removeAttribute("change");
+}
+
+function btnClick3(){
+    changeText.classList.add("change");
+}
+
+function btnClick4(){
+    changeText.classList.add("change");
+}
+
+function btnClick5(){
+    changeText.classList.add("change");
+}
+
+function btnClick6(){
+    changeText.classList.add("change");
+}
